757127ad50738e5cf2ee3e32d69b64c9e4dfaaaf
Sarah Oslund <sfoslund@microsoft.com>
2020-11-03 12:31:22 -0800

    Adding repro project

---
+++ ConsoleApp1/Parser.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.CommandLine;
+using System.CommandLine.Builder;
+
+namespace Microsoft.DotNet.Cli
+{
+    public static class Parser
+    {
+        public static readonly RootCommand RootCommand = new RootCommand();
+
+        // Subcommands
+        private static readonly Command[] Subcommands = new Command[]
+        {
+            BuildCommandParser.GetCommand()
+        };
+
+        static Parser()
+        {
+            ConfigureCommandLine();
+        }
+
+        private static void ConfigureCommandLine()
+        {
+            // Add subcommands
+            foreach (var subcommand in Subcommands)
+            {
+                RootCommand.AddCommand(subcommand);
+            }
+        }
+
+        public static System.CommandLine.Parsing.Parser Instance { get; } = new CommandLineBuilder(RootCommand)
+            //.UseExceptionHandler(ExceptionHandler)
+            .UseHelp()
+            //.UseHelpBuilder(context => new DotnetHelpBuilder(context.Console))
+            //.UseValidationMessages(new CommandLineValidationMessages())
+            .Build();
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.CommandLine.Parsing;
+
+namespace Microsoft.DotNet.Cli
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return ProcessArgs(args);
+        }
+
+        internal static int ProcessArgs(string[] args)
+        {
+            var parseResult = Parser.Instance.Parse(args);
+            Console.WriteLine(parseResult.Diagram());
+            return parseResult.Invoke();
+        }
+    }
+}
